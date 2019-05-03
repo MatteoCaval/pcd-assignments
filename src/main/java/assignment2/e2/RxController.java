bae2a33e1614e773df5de9825e10bc767f50bc04
Matteo <matteo.cavalluzzo@gmail.com>
2019-05-03 23:34:11 +0200

    first concurrent version (only addition)

--- src/main/java/assignment2/e2/RxController.java
+++ src/main/java/assignment2/e2/RxController.java
@@ -3,7 +3,9 @@ package assignment2.e2;
 import assignment2.*;
 import assignment2.e1.BusAddresses;
 import javafx.util.Pair;
+import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.schedulers.Schedulers;
 
 import java.util.Arrays;
@@ -25,20 +27,45 @@ public class RxController implements View.SelectorListener {
     public void startPressed(List<String> paths) {
         bus.putEvent(new Pair(BusAddresses.START, null));
 
+//        bus.getEvents()
+//                .filter(e -> e.getKey().equals(BusAddresses.FILE_ADDED))
+//                .map(e -> e.getValue())
+//                .observeOn(Schedulers.io())
+//                .map(e -> new Pair<>(e, DocumentAnalyzer.analyzeDocument(Document.fromPath(e))))
+//                .subscribe(new Observer<Pair<String, DocumentResult>>() {
+//                    @Override
+//                    public void onCompleted() {
+//                        Utils.log("COMPLETED");
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    @Override
+//                    public void onNext(Pair<String, DocumentResult> p) {
+//                        Utils.log("Computed element " + p.getKey());
+//                        singleResults.put(p.getKey(), p.getValue());
+//                        view.printResult(singleResults.values().stream().reduce((doc, doc2) -> DocumentResult.merge(doc, doc2)).get().toSortedPair());
+//                    }
+//                });
+
         bus.getEvents()
-                .observeOn(Schedulers.io())
                 .filter(e -> e.getKey().equals(BusAddresses.FILE_ADDED))
                 .map(e -> e.getValue())
-                .map(e -> new Pair<>(e, DocumentAnalyzer.analyzeDocument(Document.fromPath(e))))
-                .subscribe(new Observer<Pair<String, DocumentResult>>() {
+                .flatMap(name -> Observable.just(name)
+                        .observeOn(Schedulers.computation())
+                        .map(path -> new Pair<>(path, DocumentAnalyzer.analyzeDocument(Document.fromPath(path)))))
+                .subscribe(new Subscriber<Pair<String, DocumentResult>>() {
                     @Override
                     public void onCompleted() {
-                        Utils.log("COMPLETED");
+
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        e.printStackTrace();
+
                     }
 
                     @Override

db336821e286155d25808bc5b0da522c6ca51b49
Luca Giorgetti <lucagiorgit95@gmail.com>
2019-05-04 17:39:37 +0200

    fix FONDAMENTALE

--- src/main/java/assignment2/Main.java
+++ src/main/java/assignment2/Main.java
@@ -9,18 +9,19 @@ public class Main {
     public static void main(String[] args) {
         new SelectionView((mode, parallel) -> {
             MainView view = new ViewImpl();
+            String paralString = parallel ? "Parallel" : "Sequential";
             switch (mode) {
                 case ES0:
                     new ExecutorController(view, parallel);
-                    view.setFrameTitle("Assignment 2 - Tasks and executors");
+                    view.setFrameTitle("Assignment 2 - Tasks and executors - " + paralString);
                     break;
                 case ES1:
                     new VertxController(view, parallel);
-                    view.setFrameTitle("Assignment 2 - Async in the event loop");
+                    view.setFrameTitle("Assignment 2 - Async in the event loop - " + paralString);
                     break;
                 case ES2:
                     new RxController(view, parallel);
-                    view.setFrameTitle("Assignment 2 - Reactive streams");
+                    view.setFrameTitle("Assignment 2 - Reactive streams - " + paralString);
                     break;
             }
         });

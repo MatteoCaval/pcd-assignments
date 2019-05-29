4102357cb8264f892ee0b04f4989d63ea8602082
Unknown <matteo.cavalluzzo@gmail.com>
2019-05-29 23:24:03 +0200

    add action to implement

--- src/main/scala/assignment3/e0/actors/ActorActions.scala
+++ src/main/scala/assignment3/e0/actors/ActorActions.scala
@@ -1,16 +1,12 @@
 package assignment3.e0.actors
 
-import assignment3.e0.Particle
+import assignment3.e0.{P2d, Particle}
 
 object ActorActions {
 
   case class AddParticle(particle: Particle)
 
-  case class AddParticles(numberOfParticle: Int)
-
-  case class Compute(particles: Seq[Particle])
-
-  case object ComputeNext
+  case class AddParticles(numberOfParticle: Int) // not used yet
 
   case class ComputationDone(results: List[Particle])
 
@@ -18,6 +14,26 @@ object ActorActions {
 
   case class ParticleResult(particle: Particle)
 
+
+  // region messages from the ControllerActor to the ParticleActor
+
+  case class Compute(particles: Seq[Particle])
+
+  case object ComputeNext
+
+  // endregion
+
+
+  // region messages to the ControllerActor
+
   case class Start(nParticles: Int)
+
+  case object Stop
+
+  case class AddParticleByPosition(position: P2d)
+
+  case object RemoveParticle
+
+  // endregion
 
 }

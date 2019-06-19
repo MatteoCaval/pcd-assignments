ca7924a9de15a3eef959e9fce5893cc4e5d746ee
Luca Giorgetti <lucagiorgit95@gmail.com>
2019-06-19 16:06:34 +0200

    fix main assignment1

--- src/main/scala/assignment3/e1/ParticleSystemWithGUI.scala
+++ src/main/scala/assignment3/e1/ParticleSystemWithGUI.scala
@@ -2,17 +2,15 @@ package assignment3.e1
 
 object ParticleSystemWithGUI extends App {
 
-  override def main(args: Array[String]): Unit = {
-    val dt = 0.01
-    val windowSizeX = 1200
-    val windowSizeY = 1000
-    val scaleFactor = 10
+  val dt = 0.01
+  val windowSizeX = 1200
+  val windowSizeY = 1000
+  val scaleFactor = 10
 
-    val world = new World(dt)
-    val viewer = new WorldViewer(world, windowSizeX, windowSizeY, scaleFactor)
-    val controller = new Controller(world, viewer)
-    viewer.setController(controller)
-    viewer.show()
-  }
+  val world = new World(dt)
+  val viewer = new WorldViewer(world, windowSizeX, windowSizeY, scaleFactor)
+  val controller = new Controller(world, viewer)
+  viewer.setController(controller)
+  viewer.show()
 
 }
